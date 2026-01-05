bfdc6e460547ea59fe1a1445cd2a325e57b9a862
Michael-Grimm <86737735+Michael-Grimm@users.noreply.github.com>
2026-01-05 19:21:55 +0100

    Update sha512.h

--- sha512.h
+++ sha512.h
@@ -1,10 +1,9 @@
 
 /**
- * message: 
- * message_length: length in bits
- * hash: pointer to an array of eight u32 integers
+ * message_length: length in bits //max 2^64 - 1
+ * finalhash: pointer to an array of eight u64 integers
  */
-void sha512(long long int *message, long long int message_length_in_bits,long long int *hash);
+void sha512(long long int *message, long long int message_length_in_bits,long long int *finalhash);
 
 
 
